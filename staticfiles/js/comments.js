f67948218d7131b7eb18e2b742935eecc2ce3280
charlieflockhart <80785898+charlieflockhart@users.noreply.github.com>
2025-02-08 00:30:35 +0000

    Problem with CSS not updating, tried to collect staticfiles

---
+++ staticfiles/js/comments.js
@@ -0,0 +1,84 @@
+const editButtons = document.getElementsByClassName("btn-edit");
+const commentTitle_Value = document.getElementById("id_title");
+const commentLevel_Value = document.getElementById("id_level");
+const commentRating_Value = document.getElementById("id_rating");
+const commentPlat_Bool = document.getElementById("id_platinum_achieved");
+const commentPlatinumStability_Value = document.getElementById("id_platinum_stability");
+const commentGlitchedTrophies_Bool = document.getElementById("id_glitched_trophies");
+const commentGlitchedTrophiesList_Value = document.getElementById("id_glitched_trophies_list");
+const commentGame_Version_Value = document.getElementById("id_game_version");
+const commentPlaytime_Value = document.getElementById("id_playtime");
+const commentPlatform_Value = document.getElementById("id_platform");
+const commentBody_Value = document.getElementById("id_body");
+const commentForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+
+/**
+* Initializes edit functionality for the provided edit buttons.
+* 
+* For each button in the `editButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Fetches the content of the corresponding comment.
+* - Populates the `commentText` input/textarea with the comment's content for editing.
+* - Updates the submit button's text to "Update".
+* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+*/
+for (let button of editButtons) {
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("data-comment_id");
+
+    let commentTitle = document.getElementById(`title${commentId}`).innerText;
+    let commentLevel = document.getElementById(`level${commentId}`).innerText;
+    let commentRating = document.getElementById(`rating${commentId}`).innerText;
+    let commentPlatinumAchieved = document.getElementById(`platinum_achieved${commentId}`).innerText.trim().toLowerCase() === "yes";
+    let commentPlatinumStability = document.getElementById(`platinum_stability${commentId}`).innerText;
+    let commentGlitchedTrophies = document.getElementById(`glitched_trophies${commentId}`).innerText.trim().toLowerCase() === "yes";
+    let commentGlitchedTrophiesList = document.getElementById(`glitched_trophies_list${commentId}`).innerText;
+    let commentGameVersion = document.getElementById(`game_version${commentId}`).innerText;
+    let commentPlaytime = document.getElementById(`playtime${commentId}`).innerText;
+    let commentPlatform = document.getElementById(`platform${commentId}`).innerText;
+    let commentText = document.getElementById(`text${commentId}`).innerText;
+
+
+    commentTitle_Value.value = commentTitle;
+    commentLevel_Value.value = commentLevel;
+    commentRating_Value.value = commentRating;
+    commentPlat_Bool.checked = commentPlatinumAchieved;
+    commentPlatinumStability_Value.value = commentPlatinumStability;
+    commentGlitchedTrophies_Bool.checked = commentGlitchedTrophies;
+    commentGlitchedTrophiesList_Value.value = commentGlitchedTrophiesList;
+    commentGame_Version_Value.value = commentGameVersion;
+    commentPlaytime_Value.value = commentPlaytime;
+    commentPlatform_Value.value = commentPlatform;
+    commentBody_Value.value = commentText;
+
+
+
+    submitButton.innerText = "Update";
+    commentForm.setAttribute("action", `edit_comment/${commentId}`);
+  });
+}
+
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+      let commentId = e.target.getAttribute("data-comment_id");
+      deleteConfirm.href = `delete_comment/${commentId}`;
+      deleteModal.show();
+    });
+  }
