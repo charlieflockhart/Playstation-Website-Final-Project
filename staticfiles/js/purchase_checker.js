aa88b438e58704ffae15e1176310fbc36565c2e5
charlieflockhart <80785898+charlieflockhart@users.noreply.github.com>
2025-02-11 00:02:26 +0000

    All Javascript Old Code Deleted, Keeping Dev Code Comments, Collected Staticfiles

--- staticfiles/js/purchase_checker.js
+++ staticfiles/js/purchase_checker.js
@@ -1,9 +1,13 @@
-const form = document.getElementById("id_purchase_game");
-form.addEventListener('submit', function (event) {
-    const purchasedButton = document.getElementById("purchasedButton");
-    purchasedButton.innerText = "Purchased";
-    purchasedButton.disabled = true;
-});
+
+// This script is used to check if the user has purchased the game or not
+// If the user has purchased the game, the purchase button will be disabled
+// If the user has not purchased the game, the purchase button will be enabled
+
+// The script will be executed when the page is loaded
+// The script will check if the URL has a parameter named 'purchased'
+// If the parameter is 'true', the game has been purchased
+// If the parameter is 'false', the game has not been purchased
+// If the parameter is not present, the script will check the purchase status of the game
 
 window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
@@ -29,21 +33,26 @@ window.onload = function () {
 
 };
 
+// This function will be called when the page is loaded
+// This function will check if the game has been purchased
 function checkGameFirstLoad() {
-    console.log("First Load");
     document.getElementById("checkPurchasedButton").click();
 }
 
+// This function will be called when the game has been purchased
+// This function will disable the purchase button
+// The purchase button will display 'Purchased'
 function checkGamePurchasedTrue() {
-    console.log("TRUE");
     const purchaseButton = document.getElementById('purchasedButton');
     purchaseButton.textContent = 'Purchased';
     purchaseButton.disabled = true;
 
 }
 
+// This function will be called when the game has not been purchased
+// This function will enable the purchase button
+// The purchase button will display 'Purchase'
 function checkGamePurchasedFalse() {
-    console.log("FALSE");
     const purchaseButton = document.getElementById('purchasedButton');
     purchaseButton.textContent = 'Purchase';
     purchaseButton.disabled = false;
